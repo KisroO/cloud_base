58c61906ba28199e3e7c249e10c8f5471b5557bd
“ZT” <zt229298@126.com>
2022-11-01 16:34:12 +0800

    add  nacos annotation

--- gateway/src/main/java/com/kisro/cloud/GatewayApplication.java
+++ gateway/src/main/java/com/kisro/cloud/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.kisro.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Kisro
  * @since 2022/11/1
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
